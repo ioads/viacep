b8e94fa9b8f7401722dba2e87e53a5db7c67b5e6
Iago Oliveira <ioads@outlook.com>
2024-07-22 13:41:35 -0300

    cria controller

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\CepController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('search/local/{ceps}', function () {
-    return 'ok';
-});
+Route::get('search/local/{ceps}', [CepController::class, 'cep']);
